65d9f475e2bd27d3e5f4e3f8923e0d27a8f2e931
Lucas Rezende <lucasffrezende@gmail.com>
2025-03-20 00:35:01 -0300

    feat: simplify main layout by removing sidemenu items and user info

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MenuItem } from 'primeng/api';
 import { SidemenuComponent } from "../../components/sidemenu/sidemenu.component";
 
 @Component({
@@ -12,20 +11,4 @@ import { SidemenuComponent } from "../../components/sidemenu/sidemenu.component"
   styleUrl: './main-layout.component.scss'
 })
 export class MainLayoutComponent {
-
-  menuItems: MenuItem[] = [
-    { label: 'Dashboard', icon: 'pi pi-th-large', routerLink: '/dashboard' },
-    { label: 'Analytics', icon: 'pi pi-chart-line', routerLink: '/analytics', badge: '4', badgeClass: 'primary' },
-    { label: 'Usuários', icon: 'pi pi-users', routerLink: '/usuarios' },
-    { label: 'Products', icon: 'pi pi-shopping-cart', routerLink: '/products', badge: 'New', badgeClass: 'success' },
-    { label: 'Calendar', icon: 'pi pi-calendar', routerLink: '/calendar' },
-    { label: 'Messages', icon: 'pi pi-envelope', routerLink: '/messages', badge: '12', badgeClass: 'danger' },
-    { label: 'Settings', icon: 'pi pi-cog', routerLink: '/settings' }
-  ];
-
-  userInfo = {
-    name: 'John Doe',
-    role: 'Admin',
-    avatar: 'https://png.pngtree.com/png-vector/20220807/ourmid/pngtree-man-avatar-wearing-gray-suit-png-image_6102786.png'
-  };
 }
